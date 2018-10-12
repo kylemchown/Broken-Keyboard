b6fc5b9981c204f3ef070884d1d8d57f118b7f1a
kylemchown <kyle.m.chown@gmail.com>
2018-10-12 10:44:28 +0100

    Start of project

---
+++ src/main/scala/Runner.scala
@@ -0,0 +1,10 @@
+object Runner {
+  def main(args: Array[String]): Unit = {
+//    val bufferedSource = scala.io.Source.fromFile("example.txt")
+//    for (line <- bufferedSource.getLines) {
+//      println(line.toUpperCase)
+//    }
+//
+//    bufferedSource.close
+  }
+}
